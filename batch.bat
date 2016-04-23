5095b195e824e18ff561e93de71c77e5b400588d
OMMHOA <omar.osman.asd@gmail.com>
2016-04-23 23:06:46 +0200

    Results folder, batch file and tests added

---
+++ batch.bat
@@ -0,0 +1,104 @@
+javac -d bin ./game/main.java ./game/cellelements/*.java ./game/cellelements/doors/*.java ./game/map/*.java ./game/roles/*.java ./tool/*.java
+cd bin
+java game.Main 
+load ../tests/Teszt_01.txt
+exit
+java game.Main 
+load ../tests/Teszt_02.txt
+exit
+java game.Main 
+load ../tests/Teszt_03.txt
+exit
+java game.Main 
+load ../tests/Teszt_04.txt
+exit
+java game.Main 
+load ../tests/Teszt_05.txt
+exit
+java game.Main 
+load ../tests/Teszt_06.txt
+exit
+java game.Main 
+load ../tests/Teszt_07.txt
+exit
+java game.Main 
+load ../tests/Teszt_08.txt
+exit
+java game.Main 
+load ../tests/Teszt_09.txt
+exit
+java game.Main 
+load ../tests/Teszt_10.txt
+exit
+java game.Main 
+load ../tests/Teszt_11.txt
+exit
+java game.Main 
+load ../tests/Teszt_12.txt
+exit
+java game.Main 
+load ../tests/Teszt_13.txt
+exit
+java game.Main 
+load ../tests/Teszt_14.txt
+exit
+java game.Main 
+load ../tests/Teszt_15.txt
+exit
+java game.Main 
+load ../tests/Teszt_16.txt
+exit
+java game.Main 
+load ../tests/Teszt_17.txt
+exit
+java game.Main 
+load ../tests/Teszt_18.txt
+exit
+java game.Main 
+load ../tests/Teszt_19.txt
+exit
+java game.Main 
+load ../tests/Teszt_20.txt
+exit
+java game.Main 
+load ../tests/Teszt_21.txt
+exit
+java game.Main 
+load ../tests/Teszt_22.txt
+exit
+java game.Main 
+load ../tests/Teszt_23.txt
+exit
+java game.Main 
+load ../tests/Teszt_24.txt
+exit
+java game.Main 
+load ../tests/Teszt_25.txt
+exit
+java game.Main 
+load ../tests/Teszt_26.txt
+exit
+java game.Main 
+load ../tests/Teszt_27.txt
+exit
+java game.Main 
+load ../tests/Teszt_28.txt
+exit
+java game.Main 
+load ../tests/Teszt_29.txt
+exit
+java game.Main 
+load ../tests/Teszt_30.txt
+exit
+java game.Main 
+load ../tests/Teszt_31.txt
+exit
+java game.Main 
+load ../tests/Teszt_32.txt
+exit
+java game.Main 
+load ../tests/Teszt_33.txt
+exit
+java game.Main 
+load ../tests/Teszt_34.txt
+exit
